8ae217a328db090d8de6d909ab94176ab0342cd5
Zhang Tao <zhangtao@ngd.neo.org>
2019-09-11 17:50:27 +0800

    log header

--- src/unit/logbox.js
+++ src/unit/logbox.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import '../style/log.css';
 import api from '../api/logws';
 import ScrollArea from 'react-scrollbar';
-import {Combobox} from 'react-widgets';
+import { Combobox, DateTimePicker } from 'react-widgets';
+import Moment from "moment";
+import momentLocalizer from 'react-widgets-moment';
+import { Button, ButtonGroup } from "react-bootstrap";
 import 'react-widgets/dist/css/react-widgets.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+Moment.locale('en');
+momentLocalizer();
 
 class Log extends Component {
   padding(num, length) {
-    for(var len = (num + "").length; len < length; len = num.length) {
-        num = "0" + num;            
+    for (var len = (num + "").length; len < length; len = num.length) {
+      num = "0" + num;
     }
     return num;
   }
@@ -16,20 +23,20 @@ class Log extends Component {
     let index = this.padding(this.props.index, 4);
     return (
       <div className="log">
-          <p>
-            <span>
+        <p>
+          <span>
             {`${index}:[${this.props.name}] ${this.props.text}`}
-            </span>
-          </p>
+          </span>
+        </p>
       </div>
     );
   }
 }
-  
+
 class LogList extends Component {
   constructor(props) {
     super(props);
-    this.state = ({autoScroll: true});
+    this.state = ({ autoScroll: true });
   }
   componentDidUpdate() {
     if (this.state.autoScroll)
@@ -38,24 +45,24 @@ class LogList extends Component {
   onscroll = event => {
     if (!this.state.autoScroll) {
       if (event.topPosition === event.realHeight - event.containerHeight) {
-        this.setState({autoScroll: true});
+        this.setState({ autoScroll: true });
       }
     }
     if (this.state.autoScroll) {
       if (event.topPosition < event.realHeight - event.containerHeight) {
-        this.setState({autoScroll: false});
+        this.setState({ autoScroll: false });
       }
     }
   }
-  render = function() {
-    var loglistFunc = this.props.data.map(function(log) {
+  render = function () {
+    var loglistFunc = this.props.data.map(function (log) {
       return (
-        <Log text={log.Text} name={log.Name} key={log.Key} index={log.Key}/>
+        <Log text={log.Text} name={log.Name} key={log.Key} index={log.Key} />
       );
     });
     return (
       <ScrollArea
-        ref = {r => this.scrollArea = r}
+        ref={r => this.scrollArea = r}
         speed={0.8}
         className="scrollarea"
         contentClassName="loglist"
@@ -66,7 +73,7 @@ class LogList extends Component {
           "background": "white",
           "marginLeft": "0px",
         }}
-        onScroll = {this.onscroll}
+        onScroll={this.onscroll}
       >
         {loglistFunc}
       </ScrollArea>
@@ -75,55 +82,71 @@ class LogList extends Component {
 }
 
 export default class LogBox extends Component {
-    logHandler = (log, err) => {
-      if (err) {
-          console.log(err);
-          return;
-      }
-      if (this.state.tags.every(ele => ele !== log.Name)) {
-        let tags = this.state.tags;
-        tags.push(log.Name);
-        this.setState({tags: tags});
-      }
-      if (this.state.tag !== "all" && log.Name !== this.state.tag) 
-        return;
-      let index = this.state.data.length + 1;
-      if (10000 < index) {
-          log.Key = 1;
-          this.setState({data: [log]});
-          return;
-      }
-      log.Key = index;
-      let newdata = this.state.data.concat(log);
-      this.setState({data: newdata});
+  logHandler = (log, err) => {
+    if (err) {
+      console.log(err);
+      return;
     }
-    constructor(props) {
-        super(props);
-        this.state = {data: [], tag: "all", tags: ["all"]};
+    if (this.state.tags.every(ele => ele !== log.Name)) {
+      let tags = this.state.tags;
+      tags.push(log.Name);
+      this.setState({ tags: tags });
     }
-    componentWillMount() {
+    if (this.state.tag !== "all" && log.Name !== this.state.tag)
+      return;
+    let index = this.state.data.length + 1;
+    if (10000 < index) {
+      log.Key = 1;
+      this.setState({ data: [log] });
+      return;
     }
-    componentDidMount() {
-      api.Regist(this.state.tag, this.logHandler);
-    }
-  
-    render() {
-      return (
-        <div className="logBox" style={{height: 1000}}>
-          <div className="header">
-            <div>
-              <Combobox
-                defaultValue = {"all"}
-                data={this.state.tags}
-                onChange={value => {
-                  this.setState({data: [], tag: value});
-                  api.Regist(value, this.logHandler);
-                }}
-              />
-            </div>
-          </div >
-          <LogList data={this.state.data} />
-        </div>
-      );
-    }
+    log.Key = index;
+    let newdata = this.state.data.concat(log);
+    this.setState({ data: newdata });
   }
+  constructor(props) {
+    super(props);
+    this.state = { data: [], tag: "all", tags: ["all"] };
+  }
+  componentWillMount() {
+  }
+  componentDidMount() {
+    api.Regist(this.state.tag, this.logHandler);
+  }
+  render() {
+    let setRealtime = (flag) => {
+        this.setState({realtime: flag});
+    }
+    return (
+      <div className="logBox" style={{ height: 1000 }}>
+        <div className="header">
+          <Combobox
+            defaultValue={"all"}
+            data={this.state.tags}
+            onChange={value => {
+              this.setState({ data: [], tag: value });
+              api.Regist(value, this.logHandler);
+            }}
+          />
+          <ButtonGroup>
+            <Button
+              active={false}
+              onClick={() => setRealtime(true)}
+            >
+              Realtime
+            </Button>
+            <Button
+              active={this.state.realtime}
+              onClick={() => setRealtime(false)}
+            >
+              History
+            </Button>
+          </ButtonGroup>
+          <DateTimePicker format="YYYY-MM-DD kk:mm" />
+          <DateTimePicker format="YYYY-MM-DD kk:mm" />
+        </div >
+        <LogList data={this.state.data} />
+      </div>
+    );
+  }
+}
